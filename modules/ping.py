f261153a190346d4998a00e1844532c35fc6ceaa
Shaoma Uma <uma37564@gmail.com>
2011-12-05 20:18:50 -0500

    Followed TODOs for several commands, including impementing new commands and changing responses.
     deleted commands supereffective, noteffective, splash, tailwhip, and healpulse. Changed flirt's rule to respond to winks instead. Re-wrote remaining responses and implemented hugs and thanks.

--- modules/ping.py
+++ modules/ping.py
@@ -19,74 +19,38 @@ hello.rule = r'(?i)(hi|hello|hey) (I|Bli|Pi)nkie(Pie)?(Bot)?\b'
 
 def sniff(phenny, input): 
    # TODO: rewrite responses
-   sniffresponse = random.choice(('Yeah... Don\'t do that...', 'Umm...', 'Do I smell funny or something?', 'Cut that out.', 'ACTION smacks ' + input.nick + '\'s nose.'))
+   sniffresponse = random.choice(('Hey! Do I know you?', 'Umm...', 'Do I smell funny - like \'haha\' funny or \'funny\' funny?', 'EEP!', 'ACTION prods ' + input.nick + '\'s nose.'))
    phenny.say(sniffresponse)
 sniff.rule = r'(?i)(ACTION sniffs (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
 def grope(phenny, input): 
    # TODO: rewrite responses
-   groperesponse = random.choice(('Hey! Don\'t touch me there!', 'ACTION uses Flamethrower on ' + input.nick + '!', 'ACK!', 'Keep your hands off me!', 'It would probably be best if you didn\'t touch me there again.', 'How about you don\'t touch me there again and I won\'t burn your flesh off.'))
+   groperesponse = random.choice(('I love hugs!', 'ACTION hugs ' + input.nick + 'SUPER hard.', 'ACK!', 'Hahaha, you suprised me, ' + input.nick + '!', 'I don\'t think I like that touch...', 'Gasp!'))
    phenny.say(groperesponse)
 grope.rule = r'(?i)(ACTION runs over and squeezes (I|Bli|Pi)nkie(Pie)?(Bot)?)'
-
-# TODO: remove
-def supereffective(phenny, input): 
-   supereffectiveresponse = random.choice(('ACTION cries.', 'Owwwwww...', 'Ouch! That huuuuuurts....', 'Please don\'t do that...', 'ACTION screams.', 'Why did you do thaaaaat?'))
-   phenny.say(supereffectiveresponse)
-supereffective.rule = r'ACTION uses (Force Palm|Aura Sphere|Close Combat|Focus Blast|Brick Break) on (I|Bli|Pi)nkie(Pie)?(Bot)?'
-
-# TODO: remove
-def noteffective(phenny, input): 
-   noteffectiveresponse = random.choice(('ACTION claps.', 'What exactly was that supposed to accomplish?', 'That tickles a bit.', 'Your Jedi mind tricks won\'t work on me.', '...What are you doing?', 'Oh, okay. Good luck with that.'))
-   phenny.say(noteffectiveresponse)
-noteffective.rule = r'ACTION uses Psychic on (I|Bli|Pi)nkie(Pie)?(Bot)?'
-
-# TODO: remove
-def splash(phenny, input): 
-   splashresponse = random.choice(('ACTION dries herself off.', 'Great. Now my hair is wet...', 'Ah! You got my hair wet.', 'Nice Magikarp impression.', 'I\'d rather stay dry, thank you.', 'No thanks. I\'m not thirsty.'))
-   phenny.say(splashresponse)
-splash.rule = r'ACTION uses Splash on (I|Bli|Pi)nkie(Pie)?(Bot)?'
-
-# TODO: remove
-def tailwhip(phenny, input): 
-   tailwhipresponse = random.choice(('ACTION pulls ' + input.nick + '\'s tail.', 'Get your tail out of my face.', 'No, I don\'t enjoy seeing your butt. Stop that.', 'ACTION \'s defense fell!', '1,15\'\'', 'That\'s a neat trick.'))
-   phenny.say(tailwhipresponse)
-tailwhip.rule = r'ACTION uses Tail Whip on (I|Bli|Pi)nkie(Pie)?(Bot)?'
-
-# TODO: remove
-def healpulse(phenny, input): 
-   healresponse = random.choice(('Thanks, ' + input.nick + '.', 'That feels nice....', ':>', 'Thank you for that.', 'Ah...'))
-   phenny.say(healresponse)
-healpulse.rule = r'ACTION uses Heal Pulse on (I|Bli|Pi)nkie(Pie)?(Bot)?'
 
 def flirt(phenny, input): 
      # TODO: rewrite responses
      attractresponse = random.choice(('I don\'t like you that way...', 'Sorry, I\'m not like that.', 'Flattering, but no.', 'Are you trying to flirt with me?', 'I\'d rather just be friends.', '...'))
      phenny.say(attractresponse)
-flirt.rule = r'(?i)(ACTION uses (Attract|Captivate) on (I|Bli|Pi)nkie(Pie)?(Bot)?)'
+flirt.rule = r'(?i)(ACTION winks at (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 # TODO: add more rules
-
-# TODO: remove
-def attacked(phenny, input): 
-   attackedresponse = random.choice(('ACTION uses Protect!', 'Ouch!', 'ACTION uses Flamethrower on ' + input.nick + '!', 'ACTION uses Night Daze on ' + input.nick + '!', 'ACTION uses Extrasensory on ' + input.nick + '!', 'ACTION uses Counter on ' + input.nick + '!'))
-   phenny.say(attackedresponse)
-attacked.rule = r'ACTION uses (Dragon Pulse|Metal Claw|ExtremeSpeed|Quick Attack) on (I|Bli|Pi)nkie(Pie)?(Bot)?'
 
 def lick(phenny, input): 
    # TODO: Rewrite most responses
-   lickresponse = random.choice(('.', 'Ah!', 'Eep!', 'I\'ll rip out your tongue if you keep doing that.', 'ACTION rips out ' + input.nick + '\'s tongue.', 'I don\'t like you that way.', 'Eww...','Waugh...', 'Ah!', '!', 'ACTION blushes.', 'ACTION pounces ' + input.nick + '.', 'ACTION steals ' + input.nick + '\'s virginity.', 'Hee...'))
+   lickresponse = random.choice(('...', 'What do I taste like? I want to know!', 'Ah!', 'Eep!', 'That tickles!', 'ACTION licks ' + input.nick + ' right back.', 'I don\'t like you that way...', 'Eww...','Waugh...', 'Ah!', '!', 'ACTION blushes.', 'ACTION pounces ' + input.nick + '.', 'ACTION steals ' + input.nick + '\'s virginity.', 'Hehe...'))
    phenny.say(lickresponse)
 lick.rule = r'(?i)(ACTION licks (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
 def growl(phenny, input): 
    # TODO: rewrite responses
-   growlresponse = random.choice(('Bad dog! Don\'t do that.', 'ACTION smacks ' + input.nick + ' with a newspaper.', 'What did I do?', 'ACTION growls back.', 'Something wrong?'))
+   growlresponse = random.choice(('Silly puppy!', 'ACTION gives ' + input.nick + ' a slice of cake.', 'What did I do?', 'ACTION growls back.', 'Something wrong?'))
    phenny.say(growlresponse)
 growl.rule = r'(?i)(ACTION growls at (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
 def kiss(phenny, input):
     # TODO: add more responses
-    kissresponse = random.choice(('ACTION blushes','ACTION kisses back','ACTION smacks ' + input.nick + '!'))
+    kissresponse = random.choice(('ACTION blushes','What was that for?','ACTION leaps into the air in suprise.', 'ACTION pushes ' + input.nick + 'away.', 'ACTION bucks ' + input.name ' in the chest. Hard.', 'ACTION giggles.'))
     phenny.say(kissresponse)
 kiss.rule = r'(?i)(ACTION kisses (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
@@ -96,13 +60,25 @@ interjection.rule = r'(I|Bli|Pi)nkie(Pie)?(Bot)?!'
 interjection.priority = 'high'
 interjection.thread = False
 
+def hugs(phenny, input):
+   hugresponse = random.choice(('ACTION hugs ' + input.nick + ' back.', 'ACTION giggles.', 'Thanks, ' + input.nick + 'that felt really good!', 'Hugs for everypony!'))
+   phenny.say(hugresponse)
+hugs.rule = r'ACTION hugs (?i)(I|Bli|Pi)nkie(Pie)?(Bot)?'
+
+def thanks(phenny, input):
+   thanksresponse = random.choice(('No problemo', 'My pleasure', 'Hahah, you\'re welcome'))
+   thankspuncuation = random.choice(('!', '.'))
+thanks.rule = r'(?i)Thank( you|s|,)? (I|Bli|Pi)nkie(Pie)?(Bot)?'
+
 # TODO: add these actions and appropriate responses
 '''
 Actions to add:
-* hugs
 * pokes
 * party
 * cupcakes
+* kicks
+* tickles
+* pranks
 '''
 
 if __name__ == '__main__': 
